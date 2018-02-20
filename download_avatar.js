16396858815f81b0487e6d54b6cf8cc67463bdf7
Di Wu <CaptainAwesome@Di-Wu-MBP.local>
2018-02-19 18:31:08 -0800

    Step 9

--- download_avatar.js
+++ download_avatar.js
@@ -1,41 +1,42 @@
 var request = require("request");
 var fs = require("fs");
+var owner = process.argv[2];
+var repo = process.argv[3];
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb) {
+
   let options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'user-agent': 'request'
     }
   }
+
   request(options, (err, res, body) => {
     cb(err, JSON.parse(body));
   });
-
-
 }
 
 
-getRepoContributors('jquery', 'jquery', (err, result) => {
+getRepoContributors(owner, repo, (err, result) => {
 
   console.log("Errors:", err);
   for (var i = 0; i < result.length; i++) {
     console.log(result[i].avatar_url);
-    downloadImageByURL(result[i].avatar_url, './avatars/'+result[i].login+'.jpg');
+    downloadImageByURL(result[i].avatar_url, './avatars/' + result[i].login + '.jpg');
   }
-
-
 });
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
-  .on('error',(err)=> console.log('error: ', err))
-  .on('response',(response) => {
-    console.log(response.statusMessage);
-    console.log(response.headers['content-type']);})
-  .on('end',()=>console.log('Downloading Image'))
-  .pipe(fs.createWriteStream(filePath))
-  .on('finish',()=>console.log('Finished Downloading'));
+    .on('error', (err) => console.log('error: ', err))
+    .on('response', (response) => {
+      console.log(response.statusMessage);
+      console.log(response.headers['content-type']);
+    })
+    .on('end', () => console.log('Downloading Image'))
+    .pipe(fs.createWriteStream(filePath))
+    .on('finish', () => console.log('Finished Downloading'));
 }
