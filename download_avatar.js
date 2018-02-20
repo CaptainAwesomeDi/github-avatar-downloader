f3313384a22a5d269048ef912ee4afdf2d57fe55
Di Wu <CaptainAwesome@Di-Wu-MBP.local>
2018-02-19 17:37:48 -0800

    modified .gitignore for token and download_avatar.js

--- download_avatar.js
+++ download_avatar.js
@@ -1,13 +1,21 @@
-var request = require('request');
+var request = require("request");
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+console.log("Welcome to the GitHub Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  let options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'user-agent': 'request'
+    }
+  }
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
-getRepoContributors('jquery', 'jquery', (err, result) => {
-  console.log('error', err);
-  console.log('result', result);
 
+getRepoContributors('jquery', 'jquery', (err, result) => {
+  console.log("Errors:", err);
+  console.log("Result:", result);
 });
