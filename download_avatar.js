d52b0dee477f507db03f815215de5bba8cd77719
Di Wu <CaptainAwesome@Di-Wu-MBP.local>
2018-02-19 19:04:24 -0800

    added comments

--- download_avatar.js
+++ download_avatar.js
@@ -4,7 +4,9 @@ var owner = process.argv[2];
 var repo = process.argv[3];
 
 console.log("Welcome to the GitHub Avatar Downloader!");
-
+//main function to get repo contributors
+//and parse them into array of Objects
+//params: repoOwner <String> repoName <String> cb <Callback>
 function getRepoContributors(repoOwner, repoName, cb) {
   if (!(repoOwner && repoName)) {
     console.log("missing 1 or both arguments: please enter <Owner> <Repo>!")
@@ -22,9 +24,9 @@ function getRepoContributors(repoOwner, repoName, cb) {
   }
 }
 
-
+//Invoke main function with Callback function for main function
+//passes list of URL to downloadImageByURL
 getRepoContributors(owner, repo, (err, result) => {
-
   console.log("Errors:", err);
   for (var i = 0; i < result.length; i++) {
     console.log(result[i].avatar_url);
@@ -32,6 +34,8 @@ getRepoContributors(owner, repo, (err, result) => {
   }
 });
 
+//download an images and save it to a filepath
+//params: url <String> filepath <String>
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .on('error', (err) => console.log('error: ', err))
