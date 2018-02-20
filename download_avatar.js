f8c7883942d4482c6df12922c001ccb2ecc7ce5b
Di Wu <CaptainAwesome@Di-Wu-MBP.local>
2018-02-19 18:18:59 -0800

    step 7:downloading a single img by hard coding

--- download_avatar.js
+++ download_avatar.js
@@ -1,4 +1,5 @@
 var request = require("request");
+var fs = require("fs");
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
@@ -12,13 +13,28 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request(options, (err, res, body) => {
     cb(err, JSON.parse(body));
   });
+
+
 }
 
 
 getRepoContributors('jquery', 'jquery', (err, result) => {
 
   console.log("Errors:", err);
-  for(var i = 0; i < result.length; i++){
+  for (var i = 0; i < result.length; i++) {
     console.log(result[i].avatar_url);
   }
 });
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on('error',(err)=> console.log('error: ', err))
+  .on('response',(response) => {
+    console.log(response.statusMessage);
+    console.log(response.headers['content-type']);})
+  .on('end',()=>console.log('Downloading Image'))
+  .pipe(fs.createWriteStream(filePath))
+  .on('finish',()=>console.log('Finished Downloading'));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
