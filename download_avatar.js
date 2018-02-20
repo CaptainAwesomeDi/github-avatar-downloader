5efffb564ff7c75b2412dd6942075e4db1da4bc7
Di Wu <CaptainAwesome@Di-Wu-MBP.local>
2018-02-19 17:05:06 -0800

    invoke the the function with basic callback

--- download_avatar.js
+++ download_avatar.js
@@ -2,4 +2,12 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
 
+getRepoContributors('jquery', 'jquery', (err, result) => {
+  console.log('error', err);
+  console.log('result', result);
+
+});
